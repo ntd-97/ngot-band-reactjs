4f8cc0f30ea51dde3429cbcbbb6d7a1a284182e2
ntd-97 <you@example.comntdat190597@gmail.com>
2022-11-07 14:25:01 +0700

    change button props

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,10 +1,18 @@
 import React from "react";
 
-const CustomButton = ({ className, children, onClickHandler, ...props }) => {
+const CustomButton = ({
+  className,
+  children,
+  onClickHandler,
+  type = "button",
+  ...props
+}) => {
   return (
     <button
+      type={type}
       className={`${className} transition-all hover:bg-opacity-[85%]`}
       onClick={onClickHandler}
+      {...props}
     >
       {children}
     </button>
